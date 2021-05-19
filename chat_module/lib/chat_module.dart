96d1259c42a00c8b1be4e3e329e66d05b2e55e2f
arunendra <arunendra@aeologic.com>
2021-05-19 12:34:07 +0530

    Initial commit

---
+++ chat_module/lib/chat_module.dart
@@ -0,0 +1,21 @@
+library chat_module;
+
+import 'package:flutter/material.dart';
+
+class ChatHomePage extends StatefulWidget {
+  const ChatHomePage({Key key}) : super(key: key);
+
+  @override
+  _ChatHomePageState createState() => _ChatHomePageState();
+}
+
+class _ChatHomePageState extends State<ChatHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("Chat Home Page"),
+      ),
+    );
+  }
+}
